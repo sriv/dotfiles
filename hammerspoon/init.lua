3534113b18d673215f6ebc5f0ce13c85b84e5ac1
sriv <srikanth.ddit@gmail.com>
2017-03-17 14:57:42 +0530

    hammerspoon key bindings
    hyper-d : window-left hald
    hyper-g : shift window right half
    hyper-f : make window full size

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -2,7 +2,7 @@
 -- Set up
 -----------------------------------------------
 
-local hyper = {"shift","alt"}
+local hyper = {"cmd","alt", "ctrl"}
 
 hs.window.animationDuration = 0
 
@@ -19,3 +19,66 @@ function reload_config(files)
 end
 hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reload_config):start()
 show_alert("Config loaded")
+
+-----------------------------------------------
+-- hyper d for left one half window
+-----------------------------------------------
+
+hs.hotkey.bind(hyper, 'd', function()
+    if hs.window.focusedWindow() then
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
+
+        f.x = max.x
+        f.y = max.y
+        f.w = max.w / 2
+        f.h = max.h
+        win:setFrame(f)
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+-----------------------------------------------
+-- hyper g for right one half window
+-----------------------------------------------
+
+hs.hotkey.bind(hyper, 'g', function()
+    if hs.window.focusedWindow() then
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
+
+        f.x = max.x + (max.w / 2)
+        f.y = max.y
+        f.w = max.w / 2
+        f.h = max.h
+        win:setFrame(f)
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+-----------------------------------------------
+-- hyper f for fullscreen
+-----------------------------------------------
+
+hs.hotkey.bind(hyper, 'f', function()
+    if hs.window.focusedWindow() then
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
+
+        f.x = max.x
+        f.y = max.y
+        f.w = max.w
+        f.h = max.h
+        win:setFrame(f)
+    else
+        hs.alert.show("No active window")
+    end
+end)
